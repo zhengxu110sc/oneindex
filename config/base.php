937b62d00d95d87542f8de426e85c79acb2b314d
郑旭的个人主页 <38687093+zhengxu110sc@users.noreply.github.com>
2019-10-18 13:08:09 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => '郑旭的个人作品集',
+  'password' => 'oneindex',
+  'style' => 'material',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'gdclEINE28|^rikVTV952}|',
+  'client_id' => '4bd2fb5e-b014-4eb0-b60e-a521069cbeb0',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAACQN9QBRU3jT6bcBQLZNUj7UKm_f1xNl4EiEH1VrcDj1KyCB6SgoFWf7Oe7G8yB5-jvWGlQt6VqwdT1292wf-xCoYl5mrLsEkJOgeee0Tcoe3236_zhCRYDlTvv0aLHv-vQXdUjNFVCK16yMbk_gqmFybI7KF8ztHeqQbZMv6iW4FtUYQwpkjDjHbrSDKk6sn-N1wPoYTEY98O7TekPWRdQB9O0tffecsNR1wFML_CeEgbiR2bYhoOAgZ9xqbZ5Vvlvu52P2w3i87nFT2hk7i5C8RJKn4ria-YJTguXaCmW7nH-cofCY5eJXGd7KKv9eqlqOqW8LHze0HMbO8ihCr-gG4z_Bh7zsnevaNwPo1n_JKxwf3zAlNHSbgQGcqAmZJrpj_K95hpKHMYPkRspL07TWMBORQ-0k4t-Pd8SbxvT4I0vbEfvpK9KPKXFZkoeX_PZjiZp0oy6D63CARPPhlsV0VTeO1xgIB_HhgjBsJXzClRKARI_LCz6W0W9kHTdg4tHxdlOGor_1h2QGKNXZK3do10eti8ZKN0yt21pTY1AbtXlHg-tPCXkigORU3Yb1khOc2RxunoIwsatNNG-H0ARc1g9eUCYB1ehz8QSSVLi_jKQORVpCK7S-7s-rtA8XpmjRFlaffp5WbUxvjTJua6F0OPo40k4YgOBWAuAHHtwtcNLl8dB1rDtPKrDBd7Vtn6lLrdV1dZthsZZHOsBYGahTmB8bAyFYBHULAWc-C0r4hh4xXSju0WT5LF9Ghr1Rt6YzGM0f_RkylfnJBWL0SHTUtOqaFQMbwyUZE5nbeFD50vFTWoCfj3vg6QlFEh_0GgqTzuyTrgrFyaZo2aO5LkTIAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
